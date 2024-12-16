aeead0ad358d79527b67abc53107cef3cac4928f
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2024-12-15 22:28:48 -0500

    Major updates
    Major updates to data being stored

--- playercount/playercount.py
+++ playercount/playercount.py
@@ -1,24 +1,21 @@
 import csv
 import requests
 import time
+import re
 
 FIRST_TIME = 1733961600
 HEADERS = [['Timestamp', 'Region', 'Servers', 'Players']]
-REGIONS = ["USA","USA_WEST","ASIA","JAPAN","EUROPE","INDIA","AUSTRALIA","RUSSIA"]
 
-def convert_data(data, region, timestamp):
-    """
-    Converts data into a usable format
-    """
-    response = data.split(f",{region},") # splits up data by region name (easier to handle)
-    data = [part.split(",") for part in response[1:]] # removes first piece of data (unnecessary) and splits it up
-    player_count = sum(int(datum[2]) for datum in data if datum[2] != "0") # adds player count to total
-    server_count = sum(1 for datum in data if datum[2] != "0") # adds server count to total
+def convert_data(data, timestamp):
+    pattern = re.compile(r"([A-Z_]+),[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+,([0-9]+)")
+    matches = pattern.findall(data)
 
-    if server_count != 0:
-        data = [timestamp, REGIONS.index(region), server_count, player_count]
-    else:
-        data = ""
+    matches.sort(key=lambda x: x[0])
+
+    data = [int(match[1]) for match in matches]
+
+    data.insert(0, timestamp)
+
     return data
 
 def create():
@@ -50,8 +47,7 @@ with open('data/playercount.csv', mode='r') as file:
     last_timestamp = int(last_row.split(",")[0]) # gets the timestamp from that row (and converts to int)
 
 if last_timestamp < timestamp: # makes sure not doubling on data (thanks to GitHub Actions poor scheduling)
-    for region in REGIONS:
-        data = convert_data(requests.get(f"https://store2.warbrokers.io/293//server_list.php?location={region}").text, region, timestamp)
-        append(data)
+    data = requests.get("https://store1.warbrokers.io/283/get_player_list.php").text
+    append(convert_data(data, timestamp))
 
 print(f"Ran at {round(time.time())}")
