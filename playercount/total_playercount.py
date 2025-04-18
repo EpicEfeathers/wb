41f979477c8d8184a634c6a24c1e69bdbdca8aac
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2025-04-17 21:43:42 -0400

    Fix total playercount stuff

--- playercount/total_playercount.py
+++ playercount/total_playercount.py
@@ -13,6 +13,6 @@ def append(data):
         if data != "":
             csv_writer.writerow(data)  # Append a new row
 
-timestamp = round(round((round(time.time()) - FIRST_TIME)/60)/30) # gets number of half hours since original timestamp (rounded to the nearest int)
+timestamp = int(round((round(time.time()) - FIRST_TIME)/60)/30) # gets number of half hours since original timestamp (rounded to the nearest int)
 
 append((timestamp, totalPlayerCount()))
