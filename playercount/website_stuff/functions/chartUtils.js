842399b80deec1a3c03307d353b6adf159d73b13
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2025-07-09 09:21:23 -0400

    Updates to chart.js
    - Hover timestamps can now be different than the x-axis labels
    - Better way of calculating the "max" value so it looks cleaner

--- playercount/website_stuff/functions/chartUtils.js
+++ playercount/website_stuff/functions/chartUtils.js
@@ -1,29 +1,26 @@
 let myChart = null
 
-// so the chart's values look nice
-function roundToMultiple(num, multiple) {
-    return Math.ceil(num / multiple) * multiple
-}
+export function createChart( {xAxisLabels, hoverLabels, data} ) {
 
-export function createChart(labels, data) {
+    console.log(xAxisLabels, hoverLabels, data)
 
     const ctx = document.getElementById("playercount-chart")
 
-    const maxValue = Math.max(...data);
-    const yMax = maxValue === 0 ? 10 : roundToMultiple(maxValue * 1.05, 10); // add 10% padding
+    const maxValue = Math.max(...data)
+    const yMax = maxValue === 0 ? 10 : maxValue * 1.1 // add 10% padding
 
     // if chart already exists
     if (myChart) {
-        myChart.data.labels = labels;
+        myChart.data.labels = xAxisLabels;
         myChart.data.datasets[0].data = data;
-        myChart.options.scales.y.max = yMax;
+        myChart.options.scales.y.suggestedMax = yMax;
         myChart.update();
     } else {
         myChart = new Chart(ctx, {
             type: 'line',
 
             data: {
-                labels: labels,
+                labels: xAxisLabels,
                 datasets: [{
                     label: 'Player Count',
                     data: data,
@@ -33,9 +30,9 @@ export function createChart(labels, data) {
                     pointHitRadius: 10 // distance from the dot a hover popup will show
                 }]
             },
-            // only show every 2nd tick for x-values
             options: {
                 scales: {
+                    // only show every 2nd tick for x-values
                     x: {
                         ticks: {
                             callback: function (value, index) {
@@ -44,10 +41,23 @@ export function createChart(labels, data) {
                             }
                         }
                     },
+                    // max and min y values
                     y: {
                         min: 0,
                         //max: yMax
                         suggestedMax: yMax // lets the chart round up based on its own logic, keeping the max value the same step (so if counting by 50 another 50, not 20 e.g.)
+                    }
+                },
+
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                        title: function(context) {
+                            // Use index to get custom hover label
+                            const index = context[0].dataIndex;
+                            return hoverLabels[index];
+                        }
+                        }
                     }
                 }
             }
